9c208f35a12d4c37c264e516817f59a724cd549c
vineetha <vineethaveerapu4@gmail.com>
2025-05-21 11:50:05 +0200

    init

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,17 @@
+import "./index.scss";
+
+const HeroSection = () => {
+  return (
+    <div className="hero__container">
+      <div className="hero__container__content">
+        <h1>Experience Global Cuisine</h1>
+        <p>
+          Discover the finest culinary experiences and authentic flavors from
+          around the world. Your journey to exceptional dining starts here.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
